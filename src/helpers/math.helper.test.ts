2d204a2f13ceda54dc1bef097bdf89bb966f60c8
Ricardo Alberto Delgado Nava <sirdelgado@gruporevuelta.com.mx>
2025-11-10 15:16:56 -0600

    inicio testing

---
+++ src/helpers/math.helper.test.ts
@@ -0,0 +1,68 @@
+import { describe, expect, test } from "vitest";
+import { add, multiply, subtract } from "./math.helper";
+
+describe("add", () => {
+  test("should add two numbers correctly", () => {
+    // Arrange
+    const a = 2;
+    const b = 3;
+
+    // Act
+    const result = add(a, b);
+
+    // Assert
+    expect(result).toBe(a + b);
+  });
+});
+
+describe("subtract", () => {
+  test("should subtract two numbers correctly", () => {
+    // Arrange
+    const a = 2;
+    const b = 3;
+
+    // Act
+    const result = subtract(a, b);
+
+    // Assert
+    expect(result).toBe(a - b);
+  });
+
+  test("should subtract two numbers correctly", () => {
+    // Arrange
+    const a = 2;
+    const b = 0;
+
+    // Act
+    const result = subtract(a, b);
+
+    // Assert
+    expect(result).toBe(a);
+  });
+});
+
+describe("multiply", () => {
+  test("should add two numbers correctly", () => {
+    // Arrange
+    const a = 2;
+    const b = 3;
+
+    // Act
+    const result = multiply(a, b);
+
+    // Assert
+    expect(result).toBe(a * b);
+  });
+
+  test("should add two numbers correctly", () => {
+    // Arrange
+    const a = 2;
+    const b = 0;
+
+    // Act
+    const result = multiply(a, b);
+
+    // Assert
+    expect(result).toBe(b);
+  });
+});
